23d224b2f904a219109addff89174386531e06e4
MinhLV8 <leminh3214789@gmail.com>
2021-08-27 14:42:10 +0700

    update new

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Sau khi học lập trình anh là người hướng đối tượng. Và sau khi quen em, anh chuyển sang hướng về em.",
+  descriptionHeader:
+    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
+  buttonYes: "Em cũng yêu anh",
+  buttonNo: "Không! Còn lâu nhé.",
+  titleModar: "Cảm ơn em vì đồng ý",
+  descriptionModar:
+    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+};
